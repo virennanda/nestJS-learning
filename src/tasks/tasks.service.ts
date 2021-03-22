b06d9e5d1ae7f3ab5920e7a2b81013c292fa57fb
Viren Nanda <viren.nanda.01@gmail.com>
2021-03-22 17:55:21 +0530

    Session 3 complete

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task-dto';
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+  deleteTask(id: string) {
+    let newTasks = this.tasks.filter((task) => task.id !== id);
+    this.tasks = newTasks;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    let task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
